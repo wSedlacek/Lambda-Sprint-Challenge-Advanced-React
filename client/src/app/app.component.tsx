25b215a8cf6b22c2387017fa48ac69b1518b2586
William Sedlacek <wsedlacekc@gmail.com>
2019-10-04 11:46:54 -0500

    Dark Mode
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

--- client/src/app/app.component.tsx
+++ client/src/app/app.component.tsx
@@ -1,7 +1,23 @@
 import React from 'react';
 
+import { CssBaseline } from '@material-ui/core';
+import { ThemeProvider } from '@material-ui/styles';
+
+import { useLocalStorage } from './hooks/local-storage.hook';
+import { theme } from './theme/theme.config';
+import { Bar } from './components/bar/bar.component';
+import { PlayersPage } from './pages/players/players.page';
+
 const App = () => {
-  return <div></div>;
+  const [darkMode, setDarkMode] = useLocalStorage('dark-mode', false);
+
+  return (
+    <ThemeProvider theme={theme({ darkMode: darkMode })}>
+      <CssBaseline />
+      <Bar enabled={darkMode} onSwitch={setDarkMode} />
+      <PlayersPage />
+    </ThemeProvider>
+  );
 };
 
 export { App };
