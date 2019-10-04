f2635247842a06eaaf02a8843070cac321579700
William Sedlacek <wsedlacekc@gmail.com>
2019-10-04 11:45:36 -0500

    Project Setup
    Signed-off-by: William Sedlacek <wsedlacekc@gmail.com>

---
+++ client/src/app/app.component.spec.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import { App } from './app.component';
+
+test('renders without errors', () => {
+  const { baseElement } = render(<App />);
+  expect(baseElement).toBeTruthy();
+});
